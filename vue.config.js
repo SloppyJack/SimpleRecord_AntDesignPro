f0f4dfd15458e4c372fba475508ccc42c3ffa10b
SloppyJack <312883327@qq.com>
2023-11-07 19:46:51 +0800

    fix：去除使用vue的cdn节点

--- vue.config.js
+++ vue.config.js
@@ -22,18 +22,18 @@ const isProd = process.env.NODE_ENV === 'production'
 const assetsCDN = {
   // webpack build externals
   externals: {
-    vue: 'Vue',
-    'vue-router': 'VueRouter',
-    vuex: 'Vuex',
-    axios: 'axios'
+    // vue: 'Vue',
+    // 'vue-router': 'VueRouter',
+    // vuex: 'Vuex',
+    // axios: 'axios'
   },
   css: [],
   // https://unpkg.com/browse/vue@2.6.10/
   js: [
-    '//cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js',
-    '//cdn.jsdelivr.net/npm/vue-router@3.1.3/dist/vue-router.min.js',
-    '//cdn.jsdelivr.net/npm/vuex@3.1.1/dist/vuex.min.js',
-    '//cdn.jsdelivr.net/npm/axios@0.19.0/dist/axios.min.js'
+    // '//cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js',
+    // '//cdn.jsdelivr.net/npm/vue-router@3.1.3/dist/vue-router.min.js',
+    // '//cdn.jsdelivr.net/npm/vuex@3.1.1/dist/vuex.min.js',
+    // '//cdn.jsdelivr.net/npm/axios@0.19.0/dist/axios.min.js'
   ]
 }
 
